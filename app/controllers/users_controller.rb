7cc8ff233807684f63f13a548689215a740c2bd5
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-01-07 12:09:42 -0500

    User auth a go go

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest, :session_token)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  validates :username, :session_token, :password_digest, :email, presence: true, uniqueness: true
+  validates :password, length: { in: 6..20, allow_nil: true }
+  validates :username, length: { in: 4..20 }
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    (user && user.is_password?(password) ) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+end
