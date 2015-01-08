1f3a48e6b51ce7349870465f653bf6903a152a08
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-01-08 17:07:58 -0500

    Songs, yeah.

--- db/migrate/20150108195219_create_songs.rb
+++ db/migrate/20150108195219_create_songs.rb
@@ -1,10 +1,15 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :title
-      t.string :recording_id
-      
+      t.string :title, null: false
+      t.integer :recording_id, null: false
+      t.integer :play_count, null: false
+      t.string :duration, null: false
+      t.text :lyrics, null: false
       t.timestamps
     end
+    add_index :songs, :title
+    add_index :songs, :play_count
+    add_index :songs, :recording_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108171426) do
+ActiveRecord::Schema.define(version: 20150108195219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,20 @@ ActiveRecord::Schema.define(version: 20150108171426) do
     t.datetime "updated_at"
     t.text     "description",    null: false
   end
+
+  create_table "songs", force: true do |t|
+    t.string   "title",        null: false
+    t.integer  "recording_id", null: false
+    t.integer  "play_count",   null: false
+    t.string   "duration",     null: false
+    t.text     "lyrics",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "songs", ["play_count"], name: "index_songs_on_play_count", using: :btree
+  add_index "songs", ["recording_id"], name: "index_songs_on_recording_id", using: :btree
+  add_index "songs", ["title"], name: "index_songs_on_title", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username",        null: false
