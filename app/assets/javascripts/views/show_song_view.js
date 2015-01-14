12e351054a8948790803db8171374cc7279dfc47
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-01-14 11:28:02 -0500

    Destroys song likes, but can't add new ones?

--- app/assets/javascripts/views/show_song_view.js
+++ app/assets/javascripts/views/show_song_view.js
@@ -5,7 +5,8 @@ Sonido.Views.ShowSongView = Backbone.View.extend({
   template: JST["songs/show"],
   events: {
     "click .destroySong" : "destroySong",
-    "click .likeSong" : "handleLike"
+    "click .likeSong" : "createLike",
+    "click .unlikeSong" : "destroyLike"
   },
   render: function(){
     var showContents = this.template({song: this.model})
@@ -16,32 +17,44 @@ Sonido.Views.ShowSongView = Backbone.View.extend({
   renderButton: function(){
     var likeButton = this.$el.find(".songLikeStatus")
 
-
     var songLike = this.model.songLike()
     var likedStatus = this.model.songLike().get("id")
     if (!likedStatus) {
-      likeButton.html("Like this song!")
+      likeButton.html("Like this song!");
+      likeButton.data("status", "unliked");
+      likeButton.toggleClass("likeSong");
     } else {
       likeButton.html("Unlike this song!");
+      likeButton.data("status", "liked");
+      likeButton.toggleClass("unlikeSong");
     }
   },
-  createLike: function(song_id){
+  createLike: function(event){
+    var song_id = $(event.currentTarget).data("id");
+    var likeButton = this.$el.find(".songLikeStatus")
+
     var newLike = new Sonido.Models.SongLike({song_id: song_id})
     var _currentShowView = this;
     var currentSong = this;
     newLike.save({}, {
-      success: function(model){
+      success: function(model) {
         currentSong._songLike = model;
+        likeButton.toggleClass("likeSong");
         _currentShowView.render();
       }
     })
   },
-  // destroySong: function(){
-  //   this.model.destroy();
-  //   Backbone.history.navigate("", {trigger: true});
-  // },
-  handleLike: function(event){
-    var song_id = $(event.currentTarget).data("id");
+  destroyLike: function() {
+    var likeButton = this.$el.find(".songLikeStatus")
+    var _currentShowView = this;
+
+    this.model.songLike().destroy({
+      success: function(){
+        likeButton.toggleClass("unlikeSong");
+        _currentShowView.render();
+      }
+    })
+  }
 
   //   var likeButton = this.$el.find(".songLikeStatus")
   //   var likedStatus = likeButton.data("liked")
@@ -53,7 +66,6 @@ Sonido.Views.ShowSongView = Backbone.View.extend({
   //     likeButton.data("songLikeId")
   //     destroyLike(song_id);
   //   }
-  },
 
   // destroyLike: function(song_id){
   //
--- app/controllers/api/song_likes_controller.rb
+++ app/controllers/api/song_likes_controller.rb
@@ -5,6 +5,11 @@ module Api
       song_like = current_user.song_likes.new(song_params)
     end
 
+    def destroy
+      song_like = SongLike.find(params[:id])
+      song_like.destroy
+      render json: {}
+    end
 
     def song_params
       params.require(:song_like).permit(:song_id)
