efc8c5f7519e7ae9dbbe35a5331d7ccbb09f574d
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-01-29 12:14:34 -0500

    got logout callback to work

--- app/assets/javascripts/views/sonido_header.js
+++ app/assets/javascripts/views/sonido_header.js
@@ -2,9 +2,14 @@ Sonido.Views.Header = Backbone.View.extend({
   template: JST["shared/header"],
   tagName: "header",
   className: "header",
+  events: {
+    "click .logout" : "logout"
+  },
   render: function(){
     var headerContents = this.template({ currentUser: this.model });
     this.$el.html(headerContents)
     return this;
+  },
+  logout: function(){
   }
 })
--- app/assets/javascripts/views/sonido_sidebar.js
+++ app/assets/javascripts/views/sonido_sidebar.js
@@ -3,9 +3,15 @@ Sonido.Views.Sidebar = Backbone.View.extend({
   initialize: function(){
     this.listenTo(this.model, 'sync', this.render)
   },
+  events: {
+    "click .logout" : "logout"
+  },
   render: function(){
     var sidebarContents = this.template({currentUser: this.model})
     this.$el.html(sidebarContents);
     return this;
+  },
+  logout: function(){
+    
   }
 })
